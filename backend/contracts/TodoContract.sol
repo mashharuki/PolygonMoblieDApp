1deca1b695d2b95ab1fdecb69f42f5053c15fba3
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-06-11 14:43:32 +0900

    CRUD用のメソッドを実装

--- backend/contracts/TodoContract.sol
+++ backend/contracts/TodoContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 contract TodoContract {
-  
+  // counter for task id
   uint256 public taskCount = 0;
   // struct for task
   struct Task {
@@ -19,5 +19,47 @@ contract TodoContract {
   event TaskIsCompleteToggled(string task, uint256 taskId, bool isComplete);
   event TaskDeleted(uint256 taskNumber);
 
-  
+  /**
+   * create task func
+   */
+  function createTask(string memory _taskName) public {
+    // create new task
+    todos[taskCount] = Task(taskCount, _taskName, false);
+    taskCount++;
+
+    emit TaskCreated(_taskName, taskCount - 1);
+  }
+
+  /**
+   * Update task func  
+   */
+  function updateTask(uint256 _taskId, string memory _taskName) public {
+    // get task data
+    Task memory currTask = todos[_taskId];
+    // update!!
+    todos[_taskId] = Task(_taskId, _taskName, currTask.isComplete);
+
+    emit TaskUpdated(_taskName, _taskId);
+  }
+
+  /**
+   * change task state func
+   */
+  function toggleComplete(uint256 _taskId) public {
+    // get task data
+    Task memory currTask = todos[_taskId];
+    // update task state
+    todos[_taskId] = Task(_taskId, currTask.taskName, !currTask.isComplete);
+
+    emit TaskIsCompleteToggled(currTask.taskName, _taskId, !currTask.isComplete);
+  }
+
+  /**
+   * delete task func
+   */  
+  function deleteTask(uint256 _taskId) public {
+    // delete task
+    delete todos[_taskId];
+    emit TaskDeleted(_taskId);
+  }
 }
